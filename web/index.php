1d737be408743646c59cd9fcca51e464f50ce22c
RRRII <triplerdeuce@gmail.com>
2017-07-11 22:55:04 -0500

    update sql query

--- web/index.php
+++ web/index.php
@@ -2,27 +2,38 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>RRRII Data</title>
-  <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/themes/base/minified/jquery-ui.min.css" type="text/css" />
+  <title>test-product-search</title>
+  <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/themes/base/minified/jquery-ui.min.css" type="text/css" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
 </head>
-<body>
+<body style="padding:50px;">
+<div class="container" role="main">
+  <div class="jumbotron">
+    <div class="row">
+      <div class="input-group col-md-6 col-md-offset-3">
+        <input type="text" class="form-control auto" name='list_name' placeholder="Search for products...">
+      </div><!-- /input-group -->
+    </div><!-- /.row -->
+  </div>
 
+<!--
 	<form action='' method='post'>
-		<p><label>Country:</label><input type='text' name='country' value='' class='auto'></p>
+		<p><label>Product Search:</label><input type='text' name='list_name' value='' class='auto'></p>
 	</form>
-
-<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-<script type="text/javascript" src="http://code.jquery.com/ui/1.10.1/jquery-ui.min.js"></script>
+-->
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/ui/1.10.1/jquery-ui.min.js"></script>
 <script type="text/javascript">
 $(function() {
-	
+
 	//autocomplete
 	$(".auto").autocomplete({
 		source: "search.php",
 		minLength: 1
 	});
-
 });
 </script>
+</div>
 </body>
 </html>
--- web/search.php
+++ web/search.php
@@ -1,22 +1,26 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USER', 'username');
-define('DB_PASSWORD', 'password');
-define('DB_NAME', 'database name');
+// define('DB_SERVER', 'localhost');
+// define('DB_USER', 'readonly');
+// define('DB_PASSWORD', 'readonly');
+// define('DB_NAME', 'countries');
+$dsn = getenv('MYSQL_DSN');
+$user = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
 
 
 if (isset($_GET['term'])){
 	$return_arr = array();
 
 	try {
-	    $conn = new PDO("mysql:host=".DB_SERVER.";port=8889;dbname=".DB_NAME, DB_USER, DB_PASSWORD);
+		  $conn = new PDO($dsn, $user, $password);
+	    // $conn = new PDO("mysql:host=".DB_SERVER.";port=8889;dbname=".DB_NAME, DB_USER, DB_PASSWORD);
 	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	    
-	    $stmt = $conn->prepare('SELECT country FROM countries WHERE country LIKE :term');
-	    $stmt->execute(array('term' => '%'.$_GET['term'].'%'));
-	    
+
+	    $stmt = $conn->prepare('SELECT DISTINCT list_name FROM products WHERE list_name LIKE :term');
+	    $stmt->execute(array('term' => $_GET['term'].'%'));
+
 	    while($row = $stmt->fetch()) {
-	        $return_arr[] =  $row['country'];
+	        $return_arr[] =  $row['list_name'];
 	    }
 
 	} catch(PDOException $e) {
